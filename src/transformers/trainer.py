5fc3775eabb2a4c4fe663c042736330b0770cebc
Marc Sun <marc@huggingface.co>
2026-02-05 17:10:37 +0000

    remove !

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -121,6 +121,7 @@ from .trainer_utils import (
     SaveStrategy,
     TrainerMemoryTracker,
     TrainOutput,
+    align_special_tokens,
     check_target_module_exists,
     compare_trainer_and_checkpoint_args,
     default_compute_objective,
@@ -437,11 +438,8 @@ class Trainer:
                 raise RuntimeError("`Trainer` requires either a `model` or `model_init` argument")
         else:
             if model_init is not None:
-                warnings.warn(
-                    "`Trainer` requires either a `model` or `model_init` argument, but not both. `model_init` will"
-                    " overwrite your model when calling the `train` method. This will become a fatal error in the next"
-                    " release.",
-                    FutureWarning,
+                raise ValueError(
+                    "`Trainer` requires either a `model` or `model_init` argument, but not both."
                 )
             self.model_init = model_init
 
@@ -836,79 +834,6 @@ class Trainer:
         # Moving a model to an XLA device disconnects the tied weights, so we have to retie them.
         if self.args.parallel_mode == ParallelMode.TPU and hasattr(model, "tie_weights"):
             model.tie_weights()
-
-    def _align_special_tokens(self):
-        """
-        Aligns the special tokens of the tokenizer with the model configs.
-
-        A new tokens may be defined in the tokenizer for fine-tuning purposes, e.g. an "end of turn" token may be
-        added on chat models. In that case, we want the model configs to be aligned with the tokenizer, so that all
-        downstream uses work as expected. This alignment should happen before training, to ensure the prediction step
-        uses the new tokens as well.
-        """
-        if isinstance(self.processing_class, ProcessorMixin):
-            tokenizer: PreTrainedTokenizerBase = self.processing_class.tokenizer
-        else:
-            tokenizer = self.processing_class
-        model_has_generation_config = (
-            hasattr(self.model, "generation_config") and self.model.generation_config is not None
-        )
-        updated_tokens = {}
-
-        # 1 - Align EOS token. EOS is more complex than the others, as `generation_config` may hold more than one EOS
-        # token.
-        tokenizer_has_new_eos = tokenizer.eos_token_id != getattr(self.model.config, "eos_token_id", None)
-        if model_has_generation_config:
-            # `generation_config.eos_token_id` is None: direct comparison
-            if self.model.generation_config.eos_token_id is None:
-                tokenizer_has_new_eos |= tokenizer.eos_token_id != self.model.generation_config.eos_token_id
-            else:
-                # `generation_config.eos_token_id` is an `int`: convert it to list (and continue below)
-                if isinstance(self.model.generation_config.eos_token_id, int):
-                    self.model.generation_config.eos_token_id = [self.model.generation_config.eos_token_id]
-                # `generation_config.eos_token_id` is a `list`: check if the tokenizer's EOS token is in the list
-                tokenizer_has_new_eos |= tokenizer.eos_token_id not in self.model.generation_config.eos_token_id
-
-        if tokenizer_has_new_eos:
-            updated_tokens["eos_token_id"] = tokenizer.eos_token_id
-            self.model.config.eos_token_id = tokenizer.eos_token_id
-            # The generation config may hold more than one EOS token. We preserve the original EOS tokens: any of the
-            # EOS tokens defined here will halt generation.
-            if model_has_generation_config:
-                all_eos_tokens = [tokenizer.eos_token_id]
-                if self.model.generation_config.eos_token_id is not None:
-                    all_eos_tokens += list(self.model.generation_config.eos_token_id)
-                self.model.generation_config.eos_token_id = [token for token in all_eos_tokens if token is not None]
-
-        # 2 - Align BOS
-        tokenizer_has_new_bos = tokenizer.bos_token_id != getattr(self.model.config, "bos_token_id", None)
-        if model_has_generation_config:
-            tokenizer_has_new_bos |= tokenizer.bos_token_id != self.model.generation_config.bos_token_id
-
-        if tokenizer_has_new_bos:
-            updated_tokens["bos_token_id"] = tokenizer.bos_token_id
-            self.model.config.bos_token_id = tokenizer.bos_token_id
-            if model_has_generation_config:
-                self.model.generation_config.bos_token_id = tokenizer.bos_token_id
-
-        # 3 - Align PAD
-        tokenizer_has_new_pad = tokenizer.pad_token_id != getattr(self.model.config, "pad_token_id", None)
-        if model_has_generation_config:
-            tokenizer_has_new_pad |= tokenizer.pad_token_id != self.model.generation_config.pad_token_id
-
-        if tokenizer_has_new_pad:
-            updated_tokens["pad_token_id"] = tokenizer.pad_token_id
-            self.model.config.pad_token_id = tokenizer.pad_token_id
-            if model_has_generation_config:
-                self.model.generation_config.pad_token_id = tokenizer.pad_token_id
-
-        # 4 - Warn users about the changes
-        if len(updated_tokens) > 0:
-            logger.warning(
-                "The tokenizer has new PAD/BOS/EOS tokens that differ from the model config and generation config. "
-                "The model config and generation config were aligned accordingly, being updated with the tokenizer's "
-                f"values. Updated tokens: {updated_tokens}."
-            )
 
     def _set_signature_columns_if_needed(self):
         if self._signature_columns is None:
@@ -2017,7 +1942,7 @@ class Trainer:
         if isinstance(self.processing_class, (PreTrainedTokenizerBase, ProcessorMixin)) and hasattr(
             self.model, "config"
         ):
-            self._align_special_tokens()
+            align_special_tokens(self.model, self.processing_class)
 
         # Attach NEFTune hooks if necessary
         if self.neftune_noise_alpha is not None:
